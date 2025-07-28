4c78dc733e4c48bba4a2fb1ab9032697296402ea
Vedant2210 <120133071+Vedant2210@users.noreply.github.com>
2025-07-28 11:43:57 +0530

    Add files via upload

--- callback.js
+++ callback.js
@@ -1,15 +1,21 @@
-function orderPizza(callback) {
+function orderPizza(callback1,callback2) {
     console.log("Making your pizza...");
     setTimeout(() => {
-        callback("Your pizza is ready!");
+        callback1("Your pizza is ready!");
+        callback2("hi callback2");
     }, 2000);
 }
 
 
 function handleMessage(message) {
     console.log(message,"hi");
-    console.log("vedu");
-    console.log(2+3);
+ //   console.log("vedu");
+    console.log(1);
 }
 
-orderPizza(handleMessage);
+function func(message) {
+    console.log(message,"hi from 2");
+   // console.log("vedu");
+    console.log(2);
+}
+orderPizza(handleMessage,func);
